062f34cfc9aaa43e6c1f07dc92eb89429bfbd923
Kalo Pilato <kalo@Kalos-MacBook-Pro.local>
2017-04-05 10:22:18 +1200

    Added action and reducer for API index call

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
+import Promise from 'redux-promise';
 
 import routes from './routes';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
